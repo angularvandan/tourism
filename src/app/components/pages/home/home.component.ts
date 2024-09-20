6d53e291d5b6d860f95159c3ee8421491d07ed6a
angularvandan <vandankumar98522@gmail.com>
2024-09-20 21:11:38 +0530

    home page in progress

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -5,50 +5,67 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
 
   responsiveOptions: any;
-  bannerImage:any[]=[
-    {img:'../../../../assets/home/home_banner.jpg'},
-    {img:'../../../../assets/home/home_banner.jpg'},
-    {img:'../../../../assets/home/home_banner.jpg'},
-    {img:'../../../../assets/home/home_banner.jpg'}
+  bannerImage: any[] = [
+    { img: '../../../../assets/home/home_banner.jpg' },
+    { img: '../../../../assets/home/home_banner.jpg' },
+    { img: '../../../../assets/home/home_banner.jpg' },
+    { img: '../../../../assets/home/home_banner.jpg' }
   ];
-  cards:any[]=[
-    {img:'../../../../assets/home/container2/img1.png',title:'Deep Diving',subTitle:'The Philippines boasts numerous scuba diving spots that should not be missed...'},
-    {img:'../../../../assets/home/container2/img2.png',title:'Fire shows at beaches',subTitle:'The Philippines boasts numerous scuba diving spots that should not be missed...'},
-    {img:'../../../../assets/home/container2/img3.png',title:'Jump off limestones',subTitle:'Renting a kayak and paddling along the coast is one of the top things to do in Phi Phi Island tour. ..., '},
-    {img:'../../../../assets/home/container2/img1.png',title:'Deep Diving',subTitle:'The Philippines boasts numerous scuba diving spots that should not be missed...'},
-    {img:'../../../../assets/home/container2/img2.png',title:'Fire shows at beaches',subTitle:'The Philippines boasts numerous scuba diving spots that should not be missed...'},
-    {img:'../../../../assets/home/container2/img3.png',title:'Jump off limestones',subTitle:'Renting a kayak and paddling along the coast is one of the top things to do in Phi Phi Island tour. ..., '}
+  cards: any[] = [
+    { img: '../../../../assets/home/container2/img1.png', title: 'Deep Diving', subTitle: 'The Philippines boasts numerous scuba diving spots that should not be missed...' },
+    { img: '../../../../assets/home/container2/img2.png', title: 'Fire shows at beaches', subTitle: 'The Philippines boasts numerous scuba diving spots that should not be missed...' },
+    { img: '../../../../assets/home/container2/img3.png', title: 'Jump off limestones', subTitle: 'Renting a kayak and paddling along the coast is one of the top things to do in Phi Phi Island tour. ..., ' },
+    { img: '../../../../assets/home/container2/img1.png', title: 'Deep Diving', subTitle: 'The Philippines boasts numerous scuba diving spots that should not be missed...' },
+    { img: '../../../../assets/home/container2/img2.png', title: 'Fire shows at beaches', subTitle: 'The Philippines boasts numerous scuba diving spots that should not be missed...' },
+    { img: '../../../../assets/home/container2/img3.png', title: 'Jump off limestones', subTitle: 'Renting a kayak and paddling along the coast is one of the top things to do in Phi Phi Island tour. ..., ' }
   ];
+
+  // tours_package: any[] = [
+  //   {
+  //     heading: 'Boracay',
+  //     subHeading: 'Here are you must-know Boracay tips for every month!',
+  //     details: [
+  //       {
+  //         title: 'Visiting Tips',
+  //         detail: [
+  //           {
+  //             subTitle: 'Best from Nov-May', icon: '🌞'
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ];
 
 
   ngOnInit(): void {
     this.responsiveOptions = [
       {
-          breakpoint: '1199px',
-          numVisible: 4,
-          numScroll: 1
+        breakpoint: '1199px',
+        numVisible: 4,
+        numScroll: 1
       },
       {
-          breakpoint: '991px',
-          numVisible: 3,
-          numScroll: 1
+        breakpoint: '991px',
+        numVisible: 3,
+        numScroll: 1
       },
       {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 1
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 1
       }
       ,
       {
-          breakpoint: '568px',
-          numVisible: 1,
-          numScroll: 1
+        breakpoint: '568px',
+        numVisible: 1,
+        numScroll: 1
       }
-  ];
+    ];
   }
 
 }
